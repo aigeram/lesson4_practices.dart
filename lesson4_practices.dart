80cd61479c13446def16b561f915662bf825ef85
aigeram <118278771+aigeram@users.noreply.github.com>
2022-11-25 00:54:10 +0600

    lesson4_practices.dart

---
+++ lesson4_practices.dart
@@ -0,0 +1,76 @@
+
+
+void main(){
+   int a=10;
+   if(a>=10){
+   print('verno');
+   }else{
+    print('neverno');
+   }
+
+// trite1
+
+   int b=0; 
+   if(b>=0 && b>1 && b>-3){
+    print('verno');
+    }else {
+      print('neverno');
+    }
+    
+  
+
+
+    
+
+    //trite33
+
+
+    dynamic I=b>0;
+    if(b<0||b<1||b>-3){
+    print('verno');
+    }else{
+      print('verno');
+    
+  }
+
+
+    //trite4
+
+    dynamic k= a>=0;
+    if(a>=0&&a<1&&a>-3){
+      print('verno');
+    }else{
+      print('neverno');
+    }
+
+    
+
+    //trite6
+
+    dynamic u=a!=0;
+    if(a>0&& a<-3&& a>1){
+      print('verno');
+      }else{
+        print('neverno');
+      }
+
+
+      //trite7
+
+      String n ='test';
+      if(n=='test'){
+       print('verno');
+      }else{
+        print('neverno');
+      }
+
+
+      //trite8
+
+      String  s ='1';
+      if(s=='1' ){
+        print('verno');
+      }else{
+        print('neverno');
+        }
+}
